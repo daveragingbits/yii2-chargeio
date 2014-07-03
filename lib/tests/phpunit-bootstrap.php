8294c8944da1ae31b4f3638563860027b28416fc
James Sparrow <jwsparrow@gmail.com>
2014-07-03 12:42:53 -0500

    Replaced local dev configuration with placeholder for integrators

--- lib/tests/phpunit-bootstrap.php
+++ lib/tests/phpunit-bootstrap.php
@@ -3,6 +3,5 @@
 require_once 'lib/ChargeIO.php';
 require(dirname(__FILE__) . '/TestCase.php');
 
-ChargeIO::setCredentials(new ChargeIO_Credentials('m_wKgFeD0hHlaBPSGgaAQAAA', 'akZdI3PuQOWiAX9Vbrnosg000000000000000000000000000000000000000002'));
-ChargeIO::setApiUrl('http://local.chargeio.com:8080/v1');
+ChargeIO::setCredentials(new ChargeIO_Credentials('<public_key>', '<secret_key>'));
 ChargeIO::setDebug(true);
